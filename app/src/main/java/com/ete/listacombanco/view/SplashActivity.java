1842bbb0024eaf32fd8a955c3015bf8a4b606ae7
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2023-05-15 21:07:30 -0300

    finalizando tela de splash

--- app/src/main/java/com/ete/listacombanco/view/SplashActivity.java
+++ app/src/main/java/com/ete/listacombanco/view/SplashActivity.java
@@ -2,7 +2,10 @@ package com.ete.listacombanco.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.ete.listacombanco.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,5 +14,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+
+                    sleep(5000);
+
+                    Intent i=new Intent(getBaseContext(),MainActivity.class);
+                    startActivity(i);
+
+
+                    finish();
+                } catch (Exception e) {
+                    Log.e("ERRO",e.getMessage());
+                }
+            }
+        };
+        // start thread
+        background.start();
     }
 }
